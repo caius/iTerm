3e1a1d6113e9341b98534428efbccfd46e43938d
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2002-12-07 19:59:18 +0000

    subclassing NSWindowController instead of NSresponder. Eventually, I will clean up the code to make a it a true window controller.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@35 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- PseudoTerminal.h
+++ PseudoTerminal.h
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: PseudoTerminal.h,v 1.5 2002-12-06 03:26:06 yfabian Exp $
+// $Id: PseudoTerminal.h,v 1.6 2002-12-07 19:59:18 ujwal Exp $
 //
 //  PseudoTerminal.h
 //  JTerminal
@@ -18,7 +18,7 @@
 #import "VT100Screen.h"
 #import "PTYSession.h"
 
-@interface PseudoTerminal : NSResponder
+@interface PseudoTerminal : NSWindowController
 {
     /// MainMenu reference
     MainMenu *MAINMENU;
@@ -77,6 +77,7 @@
 - (void) newSession: (id) sender;
 
 - (id)init;
+- (id) initWithWindowNibName: (NSString *) windowNibName owner: (id) owner;
 - (void)dealloc;
 - (void)releaseObjects;
 

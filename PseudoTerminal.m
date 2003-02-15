cb4779f12586b7e7f8a1fb493739395b61857136
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2003-02-15 07:36:35 +0000

    using static variable in class for window count.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@271 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- PseudoTerminal.m
+++ PseudoTerminal.m
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: PseudoTerminal.m,v 1.106 2003-02-15 07:28:53 ujwal Exp $
+// $Id: PseudoTerminal.m,v 1.107 2003-02-15 07:36:35 ujwal Exp $
 //
 /*
  **  PseudoTerminal.m
@@ -54,6 +54,7 @@ static NSString *ConfigToolbarItem = @"Config";
 + (PseudoTerminal *)newTerminalWindow: (id) sender
 {
     PseudoTerminal *term;
+    static int windowCount = 0;
 
 #if DEBUG_METHOD_TRACE
     NSLog(@"%s(%d):-[PseudoTerminal newTerminal]", __FILE__, __LINE__);
@@ -65,8 +66,10 @@ static NSString *ConfigToolbarItem = @"Config";
     if ([NSBundle loadNibNamed:NIB_PATH owner:term] == NO)
 	return nil;
     // save up to 10 window positions
-    if([[NSApp windows] count] < 11)
-	[[term window] setFrameAutosaveName: [NSString stringWithFormat: @"iTerm Window %d", [[NSApp windows] count]]];
+    if(windowCount++ < 11)
+    {
+	[[term window] setFrameAutosaveName: [NSString stringWithFormat: @"iTerm Window %d", windowCount]];
+    }
     [[term window] setToolbar:[term setupToolbar]];
     #if 0
     if (lastwindow) {

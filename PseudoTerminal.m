ce16ec803d042f0e02b072f49e86864cd29d3de5
yfabian <yfabian@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2003-02-27 01:12:20 +0000

    Disable the array code by default
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@313 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- PseudoTerminal.m
+++ PseudoTerminal.m
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: PseudoTerminal.m,v 1.120 2003-02-27 00:23:46 yfabian Exp $
+// $Id: PseudoTerminal.m,v 1.121 2003-02-27 01:12:19 yfabian Exp $
 //
 /*
  **  PseudoTerminal.m
@@ -33,7 +33,7 @@
 #define DEBUG_METHOD_TRACE    0
 #define DEBUG_KEYDOWNDUMP     0
 
-#define USE_CUSTOM_DRAWING	1
+#define USE_CUSTOM_DRAWING	0
 
 #import "PseudoTerminal.h"
 #import "PTYScrollView.h"

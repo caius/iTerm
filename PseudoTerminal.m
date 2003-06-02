a8e48aae9ba87a996ba518a6f700d25e9218ef8e
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2003-06-02 16:41:02 +0000

    some more fixes to saveSession
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@537 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- PseudoTerminal.m
+++ PseudoTerminal.m
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: PseudoTerminal.m,v 1.189 2003-06-02 16:34:19 ujwal Exp $
+// $Id: PseudoTerminal.m,v 1.190 2003-06-02 16:41:02 ujwal Exp $
 //
 /*
  **  PseudoTerminal.m
@@ -1663,6 +1663,7 @@ static unsigned int windowPositions[CACHED_WINDOW_POSITIONS];
                         nil,nil);
     }
     [currentPtySession setAddressBookEntry: new];
+    [new release];
     [MAINMENU saveAddressBook];
 }
 

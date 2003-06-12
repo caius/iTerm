4e84adeafc3ebf95360c1ecdd122e5038845b6c6
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2003-06-12 16:08:56 +0000

    some experimentation.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@567 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- VT100LayoutManager.m
+++ VT100LayoutManager.m
@@ -27,6 +27,7 @@
 #import "iTerm.h"
 
 #import "VT100LayoutManager.h"
+#import "VT100Typesetter.h"
 
 #define DEBUG_METHOD_TRACE    0
 
@@ -48,10 +49,50 @@
     NSLog(@"VT100LayoutManager: aTextStorage: edited: (0x%x) range: (%d,%d) changeInLength: (%d) invalidatedRange: (%d,%d)", mask, range.location, range.length, lengthChange, invalidatedCharRange.location, invalidatedCharRange.length);
 #endif
 
+    // don't do anything if we just had attribute changes
     if(mask == NSTextStorageEditedAttributes)
 	return;
 
+#if 0
+    if (lengthChange == 0)
+    {
+	// get the glyph range
+	NSRange glyphRange;
+	unsigned nextGlyph;
+	int numLines = 0;
+
+	glyphRange = [self glyphRangeForCharacterRange: range actualCharacterRange: nil];
+	if(glyphRange.length > 0)
+	{
+	    // get the number of lines in the range
+	    int i;
+	    for (i = range.location; i < range.location + range.length; i++)
+	    {
+		if([[aTextStorage string] characterAtIndex: i] == '\n')
+		    numLines++;
+	    }
+	    if(numLines == 0)
+		numLines = 1;
+
+	    // invalidate the display for this glyph range
+	    //[self invalidateDisplayForGlyphRange: glyphRange];
+
+	    //[self invalidateGlyphsForCharacterRange:invalidatedCharRange changeInLength:0 actualCharacterRange:nil];
+	    //[self invalidateLayoutForCharacterRange:invalidatedCharRange isSoft:YES actualCharacterRange:nil];
+
+	    // call the typesetter to work on only this range
+	    [[self typesetter] layoutGlyphsInLayoutManager: self startingAtGlyphIndex: glyphRange.location maxNumberOfLineFragments: numLines nextGlyphIndex: &nextGlyph];
+
+
+	    return;
+	}
+    }
+#endif
+
+
     [super textStorage: aTextStorage edited: mask range: range changeInLength: lengthChange invalidatedRange: invalidatedCharRange];
+
+
     
 }
 

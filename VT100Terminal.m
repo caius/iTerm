4a13d15f496c56398edcba793fa5d2a6101427a5
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2003-07-20 18:55:40 +0000

    * Put back in bold face support for fonts that support it.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@659 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- VT100Terminal.m
+++ VT100Terminal.m
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: VT100Terminal.m,v 1.73 2003-07-18 15:21:13 ujwal Exp $
+// $Id: VT100Terminal.m,v 1.74 2003-07-20 18:55:40 ujwal Exp $
 //
 /*
  **  VT100Terminal.m
@@ -1930,6 +1930,24 @@ static VT100TCC decode_string(unsigned char *datap,
                 forKey:NSUnderlineStyleAttributeName];
         [dic setObject:[NSNumber numberWithInt:blink]
                 forKey:NSBlinkAttributeName];
+
+	NSFont *theFont = (i==0)?[SCREEN font]:[SCREEN nafont];
+	if(bold == 1)
+	{
+	    // convert font to bold face
+	    NSFont *boldFont;
+	    
+	    boldFont = [[NSFontManager sharedFontManager] convertFont: theFont toHaveTrait: NSBoldFontMask];
+	    // make sure the conversion was successful
+	    if([boldFont isFixedPitch])
+		[dic setObject: boldFont forKey: NSFontAttributeName];
+	    else
+		[dic setObject: theFont forKey: NSFontAttributeName];
+	}
+	else
+	{
+	    [dic setObject: theFont forKey: NSFontAttributeName];
+	}
     }
 
 }

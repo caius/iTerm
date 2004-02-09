a03a892bc4e645f3e2cc0f09d78001a42e470ff0
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2004-02-09 08:36:37 +0000

    new proposed UI for custom key binding.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@917 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

---
+++ Headers/iTerm/iTermKeyBindingMgr.h
@@ -0,0 +1,62 @@
+/*
+ **  iTermKeyBindingMgr.h
+ **
+ **  Copyright (c) 2002, 2003
+ **
+ **  Author: Ujwal S. Sathyam
+ **
+ **  Project: iTerm
+ **
+ **  Description: Header file for key binding manager.
+ **
+ **  This program is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 2 of the License, or
+ **  (at your option) any later version.
+ **
+ **  This program is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with this program; if not, write to the Free Software
+ **  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+// Actions for key bindings
+#define KEY_ACTION_NEXT_SESSION			0
+#define KEY_ACTION_NEXT_WINDOW			1
+#define KEY_ACTION_PREVIOUS_SESSION		2
+#define KEY_ACTION_PREVIOUS_WINDOW		3
+#define KEY_ACTION_SCROLL_LINE_DOWN		4
+#define KEY_ACTION_SCROLL_LINE_UP		5
+#define KEY_ACTION_SCROLL_PAGE_DOWN		6
+#define KEY_ACTION_SCROLL_PAGE_UP		7
+#define KEY_ACTION_ESCAPE_SEQUENCE		8
+#define KEY_ACTION_HEX_CODE				9
+
+
+@interface iTermKeyBindingMgr : NSObject {
+	NSDictionary *profiles;
+	NSDictionary *currentProfile;
+}
+
+// Class methods
++ (id) sharedInstance;
+
+// Instance methods
+- (id) init;
+- (void) dealloc;
+
+- (NSDictionary *) profiles;
+- (void) setProfiles: (NSDictionary *) aDict;
+- (NSDictionary *) currentProfile;
+- (void) setCurrentProfile: (NSDictionary *) aDict;
+
+- (int) actionForKeyEvent: (NSEvent *) anEvent escapeSequence: (NSString **) escapeSequence hexCode: (int *) hexCode;
+- (int) entryAtIndex: (int) index key: (NSString *) unmodkeystr modifiers: (int *) modifiers;
+
+@end
---
+++ iTermKeyBindingMgr.m
@@ -0,0 +1,14 @@
+//
+//  iTermKeyBindingMgr.m
+//  iTerm
+//
+//  Created by Ujwal Setlur on Sun Feb 08 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <iTerm/iTermKeyBindingMgr.h>
+
+
+@implementation iTermKeyBindingMgr
+
+@end

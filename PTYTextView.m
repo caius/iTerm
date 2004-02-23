66e684a48aefeb38600f2bf640af1b72dfc1cd47
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2004-02-23 07:40:15 +0000

    * Making sure dirty flags are reset for a line after processing it.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@965 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- PTYTextView.m
+++ PTYTextView.m
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: PTYTextView.m,v 1.132 2004-02-22 22:47:15 ujwal Exp $
+// $Id: PTYTextView.m,v 1.133 2004-02-23 07:40:15 ujwal Exp $
 /*
  **  PTYTextView.m
  **
@@ -606,7 +606,7 @@
 	NSRect bgRect;
 	NSColor *aColor;
 	char  *fg, *bg, *dirty;
-	BOOL need_draw;
+	BOOL need_draw, lineWasAltered;
 	int bgstart, ulstart;
     float curX, curY;
 	char bgcode, sel, fgcode;
@@ -690,12 +690,15 @@
 			dirty=[dataSource dirty]+lineIndex*WIDTH;
 		}	
 		
+		lineWasAltered = NO;
+		
 		//draw background and underline here
 		bgstart=ulstart=-1;
 		for(j=0;j<WIDTH;j++) {
 			if (buf[j]==0xffff) continue;
 			// Check if we need to redraw next char
 			need_draw = line < startScreenLineIndex || forceUpdate || dirty[j] || (fg[j]&BLINK_MASK);
+			//NSLog(@"line %d; cell %d: forceUpdate = %d; dirty = %d", line, j, forceUpdate, dirty[j]);
 			// find out if the current char is being selected
 			sel=(x1 != -1 && x2 != -1 &&
 				 ((line > y1 && line < y2) ||
@@ -706,9 +709,11 @@
 			// if we don't have to update next char, finish pending jobs
 			if (!need_draw){
 				if (bgstart>=0) {
+					lineWasAltered = YES;
 					aColor = (bgcode>=0)? [self colorForCode:bgcode] : selectionColor; 
 					[aColor set];
-									
+					
+					//NSLog(@"1. line %d: filling %d cells", line, j-bgstart); 
 					bgRect = NSMakeRect(curX+bgstart*charWidth,curY-lineHeight,(j-bgstart)*charWidth,lineHeight);
 					// if we have a background image and we are using the default background color, redraw image
 					if([(PTYScrollView *)[self enclosingScrollView] backgroundImage] != nil && [aColor isEqual: defaultBGColor])
@@ -727,11 +732,17 @@
 				bgstart=ulstart=-1;
 			}
 			else {
-				if (bgstart<0) { bgstart=j; bgcode=sel; }
+				if (bgstart<0) 
+				{
+					bgstart=j; 
+					bgcode=sel; 
+				}
 				else if (sel!=bgcode) {
+					lineWasAltered = YES;
 					aColor = (bgcode>=0)? [self colorForCode:bgcode] : selectionColor; 
 					[aColor set];
 					
+					//NSLog(@"2. line %d: filling %d cells", line, j-bgstart); 
 					bgRect = NSMakeRect(curX+bgstart*charWidth,curY-lineHeight,(j-bgstart)*charWidth,lineHeight);					
 					// if we have a background image and we are using the default background color, redraw image
 					if([(PTYScrollView *)[self enclosingScrollView] backgroundImage] != nil && [aColor isEqual: defaultBGColor])
@@ -747,6 +758,7 @@
 				}
 				if (ulstart<0 && fg[j]&UNDER_MASK && buf[j]) { ulstart=j; fgcode=fg[j]; }
 				else if (ulstart>=0 && (fg[j]!=fgcode || !buf[j])) {
+					lineWasAltered = YES;
 					[[self colorForCode:fgcode] set];
 					NSRectFill(NSMakeRect(curX+ulstart*charWidth,curY-2,(j-ulstart)*charWidth,1));
 					fgcode=fg[j];
@@ -754,10 +766,12 @@
 				}
 			}
 		}
+		//NSLog(@"bgstart for line %d = %d", line, bgstart);
 		if (bgstart>=0) {
 			aColor = (bgcode>=0)? [self colorForCode:bgcode] : selectionColor; 
 			[aColor set];
 			
+			//NSLog(@"3. line %d: filling %d cells", line, j-bgstart); 
 			bgRect = NSMakeRect(curX+bgstart*charWidth,curY-lineHeight,(j-bgstart)*charWidth,lineHeight);
 			
 			// if we have a background image and we are using the default background color, redraw image
@@ -779,18 +793,23 @@
 		//draw all char
 		for(j=0;j<WIDTH;j++) {
 			need_draw = (buf[j] && buf[j]!=0xffff) && (line < startScreenLineIndex || forceUpdate || dirty[j] || (fg[j]&BLINK_MASK));
-			if (need_draw) { 	
+			if (need_draw) { 
+				//NSLog(@"drawing char %d for line %d; forceUpdate = %d; dirty = %d", j, line, forceUpdate, dirty[j]);
 				[self drawCharacter:buf[j] fgColor:fg[j] AtX:curX Y:curY];
-				if (fg[j]&BLINK_MASK) { //if blink is set, switch the fg/bg color
-					t=fg[j]&0x1f;
-					fg[j]=(fg[j]&0xe0)+bg[j];
-					bg[j]=t;
-				}
-				else if(line>=startScreenLineIndex) 
-					dirty[j]=0;
 			}
+						
+			// reset dirty flag on this line
+			if (buf[j] && (fg[j]&BLINK_MASK)) { //if blink is set, switch the fg/bg color
+				t=fg[j]&0x1f;
+				fg[j]=(fg[j]&0xe0)+bg[j];
+				bg[j]=t;
+			}
+			else if(line>=startScreenLineIndex) 
+				dirty[j]=0;				
+			
 			curX+=charWidth;
 		}
+		
 		//if (line>=startScreenLineIndex) memset(dirty,0,WIDTH);
 		curY+=lineHeight;
 	}

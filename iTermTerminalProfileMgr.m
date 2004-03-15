d771603447825d9434ed2a3881e6496bda8a0a56
ujwal <ujwal@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2004-03-15 15:46:12 +0000

    added GPL header.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@1063 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- iTermTerminalProfileMgr.m
+++ iTermTerminalProfileMgr.m
@@ -1,10 +1,29 @@
-//
-//  iTermTerminalProfileMgr.m
-//  iTerm
-//
-//  Created by Tianming Yang on Sun Mar 14 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
-//
+/*
+ **  iTermTerminalProfileMgr.m
+ **
+ **  Copyright (c) 2002, 2003, 2004
+ **
+ **  Author: Tianming Yang
+ **
+ **  Project: iTerm
+ **
+ **  Description: terminal profile manager.
+ **
+ **  This program is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 2 of the License, or
+ **  (at your option) any later version.
+ **
+ **  This program is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with this program; if not, write to the Free Software
+ **  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 
 #import "iTerm/iTermTerminalProfileMgr.h"
 

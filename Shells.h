9b22776fb9e2c8a59f2331c4b6103de5968e22f8
dnedrow <dnedrow@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2007-03-19 20:14:17 +0000

    Shells.m provide basic access to /etc/shells. This calss is primarily for use with iTermSecurityManager.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@1529 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

---
+++ Shells.h
@@ -0,0 +1,41 @@
+// -*- mode:objc -*-
+// $Id: Shells.h,v 1.1 2007-03-19 20:14:17 dnedrow Exp $
+//
+/*!
+	@class Shells
+	
+	This class encapsulates access to /etc/shells
+	
+	Copyright (c) 2007
+	
+	Author: David E. Nedrow
+	
+	Project: iTerm
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+
+@interface Shells : NSObject {
+
+}
+
++ (id) sharedInstance;
+
+- (NSString *) getShells;
+
+@end
---
+++ Shells.m
@@ -0,0 +1,89 @@
+// -*- mode:objc -*-
+// $Id: Shells.m,v 1.1 2007-03-19 20:14:17 dnedrow Exp $
+//
+/*!
+@class Shells
+
+This class encapsulates access to /etc/shells
+
+Copyright (c) 2007
+
+Author: David E. Nedrow
+
+Project: iTerm
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#import "Shells.h"
+
+/*!
+@interface Shells (PrivateMethods )
+@abstract Category extends Shells with private methods
+@discussion Any methods that should not be exposed to the 'outside world'
+by being included in the class header file should be declared here and 
+implemented in the implementation section.
+*/
+@interface Shells (PrivateMethods )
+NSString *shellsPath = @"/etc/shells";
+NSString *shells;
+- (BOOL) loadShells;
+@end
+
+@implementation Shells
+
++ (id) sharedInstance {
+	static Shells* shared = nil;
+	if(!shared)
+		shared = [Shells new];
+	return shared;
+}
+
+- (id) init {
+	if ((self = [super init])) {
+		[self loadShells];
+		return self;
+	} else {
+		return nil;
+	}
+}
+
+- (void) dealloc {
+	[super dealloc];
+}
+
+- (BOOL) loadShells {
+	
+	shells = [NSString stringWithContentsOfFile:shellsPath];
+	if (!shells) return (NO);
+	
+#if DEBUG_ALLOC
+	NSArray *shellArray = [shells componentsSeparatedByString:@"\n"];
+	
+	NSEnumerator *nse = [shellArray objectEnumerator];
+	
+	while(shell = [nse nextObject]) {
+		NSLog(shell);
+	}
+#endif
+	
+	return (YES);
+}
+
+- (NSString *) getShells {
+	return shells;
+}
+
+@end

1817d3a4ab31c9861f5b6e52d45287ab21bc3a7f
dnedrow <dnedrow@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2007-04-02 17:48:25 +0000

    Initialized NSMutableSet and added local call to reload /etc/shells each time getShells is called.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@1534 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- Shells.m
+++ Shells.m
@@ -1,5 +1,5 @@
 // -*- mode:objc -*-
-// $Id: Shells.m,v 1.3 2007-04-02 17:18:31 dnedrow Exp $
+// $Id: Shells.m,v 1.4 2007-04-02 17:48:25 dnedrow Exp $
 //
 /*!
 @class Shells
@@ -73,12 +73,16 @@ NSString *shells;
 }
 
 - (NSSet *) getShells {
-	if (!shells) {
-		return (NULL);
+	
+	NSMutableSet *shellSet = [NSMutableSet set];
+
+	// We want to ping /etc/shells each time getShells is called, just in
+	// case the admin updates /etc/shells while iTerm is running.
+	if ((![self loadShells]) || ([shells length] <= 0)) {
+		return (shellSet);
 	}
 	
 	NSEnumerator *shellEnum = [[shells componentsSeparatedByString:@"\n"] objectEnumerator];
-	NSMutableSet *shellSet;
 	NSString *shell;
 	
 	while((shell = [shellEnum nextObject])) {

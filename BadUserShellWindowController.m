27cf2c4e819144ccdbe880d1c2a20b4882378711
dnedrow <dnedrow@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2008-01-30 03:50:08 +0000

    Initial GUI for bad shell warning.
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@1551 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

---
+++ BadUserShellWindowController.m
@@ -0,0 +1,14 @@
+//
+//  BadUserShellWindowController.m
+//  iTerm
+//
+//  Created by David Nedrow on 6/23/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "BadUserShellWindowController.h"
+
+
+@implementation BadUserShellWindowController
+
+@end

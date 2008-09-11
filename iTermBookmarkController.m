6ea8946792d107c5890f42ec3d409a71217a2278
yfabian <yfabian@a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9>
2008-09-11 05:19:41 +0000

    Add bookmark dialog now clears the text entries, and automatically fills profiles from the selected bookmark (#1815642).
    git-svn-id: https://iterm.svn.sourceforge.net/svnroot/iterm/trunk@1613 a86d2aa4-6e2d-4ff4-9c10-3c044852e7c9

--- iTermBookmarkController.m
+++ iTermBookmarkController.m
@@ -266,6 +266,30 @@ static BOOL editingBookmark = NO;
 	// load our profiles
 	[self _loadProfiles];
 	
+	[bookmarkName setStringValue: @""];
+	[bookmarkCommand setStringValue: @""];
+	[bookmarkWorkingDirectory setStringValue: @""];
+	[bookmarkShortcut selectItemWithTitle: @""];
+
+	if ([bookmarksView selectedRow]!=-1) {
+		id selectedItem = [bookmarksView itemAtRow: [bookmarksView selectedRow]];
+		NSString *terminalProfile = [[ITAddressBookMgr sharedInstance] objectForKey: KEY_TERMINAL_PROFILE inItem: selectedItem];
+		NSString *keyboardProfile = [[ITAddressBookMgr sharedInstance] objectForKey: KEY_KEYBOARD_PROFILE inItem: selectedItem];
+		NSString *displayProfile = [[ITAddressBookMgr sharedInstance] objectForKey: KEY_DISPLAY_PROFILE inItem: selectedItem];
+		
+		if([bookmarkTerminalProfile indexOfItemWithTitle: terminalProfile] < 0)
+			terminalProfile = NSLocalizedStringFromTableInBundle(@"Default",@"iTerm", [NSBundle bundleForClass: [self class]], @"Terminal Profiles");
+		[bookmarkTerminalProfile selectItemWithTitle: terminalProfile];
+		
+		if([bookmarkKeyboardProfile indexOfItemWithTitle: keyboardProfile] < 0)
+			keyboardProfile = NSLocalizedStringFromTableInBundle(@"Global",@"iTerm", [NSBundle bundleForClass: [self class]], @"Key Binding Profiles");
+		[bookmarkKeyboardProfile selectItemWithTitle: keyboardProfile];
+		
+		if([bookmarkDisplayProfile indexOfItemWithTitle: displayProfile] < 0)
+			displayProfile = NSLocalizedStringFromTableInBundle(@"Default",@"iTerm", [NSBundle bundleForClass: [self class]], @"Display Profiles");
+		[bookmarkDisplayProfile selectItemWithTitle: displayProfile];
+	}
+
 	[NSApp beginSheet: editBookmarkPanel
 	   modalForWindow: [self window]
 		modalDelegate: self
